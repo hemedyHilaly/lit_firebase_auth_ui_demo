07d2702692d019cbc08019687c7b92a7ce945234
hemedyHilaly <hemedyhilaly25@gmail.com>
2021-11-07 20:48:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lit_firebase_auth/lit_firebase_auth.dart';
+import 'package:lit_firebase_ui_demo/config/palette.dart';
+import 'package:lit_firebase_ui_demo/screens/splash.dart';
+
+import 'screens/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LitAuthInit(
+      authProviders: const AuthProviders(
+        emailAndPassword: true,
+        google: true,
+        apple: true,
+        twitter: true,
+      ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.muliTextTheme(),
+          accentColor: Palette.darkOrange,
+          appBarTheme: const AppBarTheme(
+            brightness: Brightness.dark,
+            color: Palette.darkBlue,
+          ),
+        ),
+
+        home: const LitAuthState(
+          authenticated: HomeScreen(),
+          unauthenticated: SplashScreen(),
+        ),
+        //home: const SplashScreen(),
+      ),
+    );
+  }
+}
